6be3dd17ad8bceaa3e13e60ff63117dd908ebc08
reeeeeyy <koysborjigss@gmail.com>
2023-04-27 15:08:30 +0800

    initial Commit

---
+++ 01-variable-datatypes/01console/script.js
@@ -0,0 +1,28 @@
+//alert("Hello from JS File");
+
+// this is how to log a number
+console.log(100);
+
+// this is how to log string
+console.log("Hello World");
+
+// this is how to log multiple values
+console.log(20, "Hello", true);
+
+// this is how to log a variable
+const x = 100;
+console.log(x);
+
+// console error & warning
+console.error("Alert");
+console.warn("Warning");
+
+// lob object as a table
+console.table({ name: "Rey", email: "rey@gmail.com" });
+
+// Group console commands
+console.group("simple");
+console.log(x);
+console.error("Alert");
+console.warn("Warning");
+console.groupEnd();
