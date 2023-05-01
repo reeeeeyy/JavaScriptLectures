549c87182dca0e08af4a718a3168090409407361
reeeeeyy <koysborjigss@gmail.com>
2023-05-01 12:57:37 +0800

    Initial Commit

---
+++ 02-arrays-and-objects/04challenge3-arrays/script.js
@@ -0,0 +1,28 @@
+//Challenge 3.1
+const arr = [1, 2, 3, 4, 5];
+arr.reverse();
+arr.unshift(6);
+arr.push(0);
+
+//result
+console.log(arr);
+
+//Challenge 3.2
+
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [5, 6, 7, 8, 9, 10];
+
+arr1.splice(4, 5);
+// console.log(arr1);
+
+//Using concat
+const arr3 = arr1.concat(arr2);
+
+//Result of concat
+console.log(arr3);
+
+//Using ... spread
+const arr4 = [...arr1, ...arr2];
+
+//Result of ...spread
+console.log(arr4);
